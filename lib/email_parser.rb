d70cf7e8bfb97472a144abe742ff7a9f9c206e2d
Judy McGoldrick <jjeanius@verizon.net>
2018-01-10 22:14:59 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,6 +14,7 @@ attr_accessor :name, :emails
 #  end
 
   def self.parse(address)
+    binding.pry
     @@all.collect |email| email.name == address
     address.split(" , ")[1],split(" @ .com")
     end
